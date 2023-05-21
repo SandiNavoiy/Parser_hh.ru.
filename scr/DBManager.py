422f23a7827668b395e30cb6f739e0c2cd4c179a
Sandi_Navoiy <santes.au@gmail.com>
2023-05-21 23:11:58 +0300

    косметика

--- scr/DBManager.py
+++ scr/DBManager.py
@@ -3,7 +3,9 @@ import psycopg2
 
 class DBManage:
     """Класс  для работы с данными в БД"""
-    pass
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname="head_hanter", user="postgres", password="1", host="localhost")
+        self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count():
         """Получает список всех компаний и количество вакансий у каждой компании"""
@@ -26,3 +28,7 @@ class DBManage:
         """Получает список всех вакансий, в названии которых содержатся переданные
         в метод слова, например “python”"""
         pass
+    def close_connection(self):
+        """Закрытие соединения с базой данных"""
+        self.cur.close()
+        self.conn.close()
--- util/util.py
+++ util/util.py
@@ -8,7 +8,7 @@ def customers(file_mame):
     with open(os.path.join(file_mame), 'r') as f:
         reader = csv.reader(f)
         next(reader)
-        conn = psycopg2.connect(dbname="north", user="postgres", password="1", host="localhost")
+        conn = psycopg2.connect(dbname="head_hanter", user="postgres", password="1", host="localhost")
         cur = conn.cursor()
         for row in reader:
             cur.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",

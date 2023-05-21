ff75f01e9e46fbb0083ee0acd5d6180bd962853b
Sandi_Navoiy <santes.au@gmail.com>
2023-05-21 17:30:59 +0300

    косметика

--- scr/abc.py
+++ scr/abc.py
@@ -13,38 +13,3 @@ class Employer(ABC):
     def get_vacancies(self):
         pass
 
-
-class VacancyStorage(ABC):
-    """Абстрактный класс для классов работы с данными вакансий"""
-
-    @abstractmethod
-    def sorting(self, vacancy):
-        pass
-
-    @abstractmethod
-    def list_of_vacancy(self):
-        pass
-
-    @abstractmethod
-    def get_vacancies(self, **kwargs):
-        pass
-
-    @abstractmethod
-    def top(self, vacancy):
-        pass
-
-
-class JsonSave(ABC):
-    """ Абстрактный класс работы с файлами json"""
-
-    @abstractmethod
-    def add_vacancy(self):
-        pass
-
-    @abstractmethod
-    def remove_vacancy(self):
-        pass
-
-    @abstractmethod
-    def clean_file_favourites(self):
-        pass

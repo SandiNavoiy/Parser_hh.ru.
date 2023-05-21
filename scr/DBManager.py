48fb827c7c7b0c3ec0ac154b5845f4bfc0696a50
Sandi_Navoiy <santes.au@gmail.com>
2023-05-21 23:22:52 +0300

    косметика

--- scr/DBManager.py
+++ scr/DBManager.py
@@ -8,24 +8,24 @@ class DBManage:
         self.conn = psycopg2.connect(dbname="head_hanter", user="postgres", password="1", host="localhost")
         self.cur = self.conn.cursor()
 
-    def get_companies_and_vacancies_count():
+    def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании"""
         pass
 
-    def get_all_vacancies():
+    def get_all_vacancies(self):
         """Получает список всех вакансий с указанием
           названия компании, названия вакансии и зарплаты и ссылки на вакансию"""
         pass
 
-    def get_avg_salary():
+    def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям."""
         pass
 
-    def get_vacancies_with_higher_salary():
+    def get_vacancies_with_higher_salary(self):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
         pass
 
-    def get_vacancies_with_keyword():
+    def get_vacancies_with_keyword(self):
         """Получает список всех вакансий, в названии которых содержатся переданные
         в метод слова, например “python”"""
         pass

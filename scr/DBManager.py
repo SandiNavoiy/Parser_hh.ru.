f0ac6a806961e7cfd2fb31804087677f939898b9
Alex <santes.au@gmail.com>
2023-05-24 13:53:44 +0300

    Черновой не рабочий вариант класса DBManage

--- scr/DBManager.py
+++ scr/DBManager.py
@@ -36,27 +36,124 @@ class DBManage:
                     "FOREIGN KEY (employer_id) REFERENCES employers (id))")
         conn.close()
 
+    def insert_employer(self, name, description, website):
+        """"Вставка данных о работодателе в таблицу employers"""
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO employers (name, description, website)
+            VALUES (%s, %s, %s)
+            RETURNING id
+            """,
+            (name, description, website)
+        )
+        employer_id = cur.fetchone()[0]
+        conn.close()
+        return employer_id
+
+    def insert_vacancy(self, employer_id, title, salary, link):
+        """Вставка данных о вакансии в таблицу vacancies"""
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO vacancies (employer_id, title, salary, link)
+            VALUES (%s, %s, %s, %s)
+            """,
+            (employer_id, title, salary, link)
+        )
+        conn.close()
+
+
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        pass
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT employers.name, COUNT(vacancies.id)
+            FROM employers
+            LEFT JOIN vacancies ON employers.id = vacancies.employer_id
+            GROUP BY employers.name
+            """
+        )
+        result = cur.fetchall()
+        conn.close()
+        return result
 
     def get_all_vacancies(self):
         """Получает список всех вакансий с указанием
           названия компании, названия вакансии и зарплаты и ссылки на вакансию"""
-        pass
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT employers.name, vacancies.title, vacancies.salary, vacancies.link
+            FROM vacancies
+            INNER JOIN employers ON vacancies.employer_id = employers.id
+            """
+        )
+        result = cur.fetchall()
+        conn.close()
+        return result
 
     def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям."""
-        pass
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT AVG(CAST(REPLACE(salary, 'руб.', '') AS INTEGER))
+            FROM vacancies
+            """
+        )
+        result = cur.fetchone()[0]
+        conn.close()
+        return result
 
     def get_vacancies_with_higher_salary(self):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        pass
+        avg_salary = self.get_avg_salary()
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT employers.name, vacancies.title, vacancies.salary, vacancies.link
+            FROM vacancies
+            INNER JOIN employers ON vacancies.employer_id = employers.id
+            WHERE CAST(REPLACE(vacancies.salary, 'руб.', '') AS INTEGER) > %s
+            """,
+            (avg_salary,)
+        )
+        result = cur.fetchall()
+        conn.close()
+        return result
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, keyword):
         """Получает список всех вакансий, в названии которых содержатся переданные
         в метод слова, например “python”"""
-        pass
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT employers.name, vacancies.title, vacancies.salary, vacancies.link
+            FROM vacancies
+            INNER JOIN employers ON vacancies.employer_id = employers.id
+            WHERE vacancies.title ILIKE %s
+            """,
+            ('%' + keyword + '%',)
+        )
+        result = cur.fetchall()
+        conn.close()
+        return result
 
     def close_connection(self):
         """Закрытие соединения с базой данных"""
